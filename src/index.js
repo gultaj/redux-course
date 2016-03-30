bd0e40bd0d9af8a4c0374b7a2e5781bfe323ec82
gultaj <yaschick.mail@gmail.com>
2016-03-31 00:56:34 +0300

    add redux. project structure

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './containers/App';
 
 render(<App />, document.getElementById('root'));
